e66be7cb47652988c0de87bd115a5d8a0048d818
drakkar0 <drakkar0studio@gmail.com>
2023-10-31 10:56:09 +0200

    31,10

---
+++ downloader/main.py
@@ -0,0 +1,98 @@
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseDownload
+from google.oauth2 import service_account
+import io
+from pathlib import Path
+import time
+
+def get_folder_files(dirid):
+    results = drive_service.files().list(q=f"'{dirid}' in parents", fields='files(id, name)').execute()
+    return results.get('files', [])
+
+
+
+starttime = time.time()
+
+print(starttime)
+
+pathtodownload = Path('D:/Devil/cryptomaraphone')
+
+if not pathtodownload.exists():
+    pathtodownload.mkdir()
+
+
+#настройки
+path_to_key = 'D:/GIT/python/downloader/' #папка с сектретным файлом
+secret_key = 'peak-essence-291819-560cc749a978.json' #Секретный файл
+folder_id = '1HTL9h_EXCmh43ES8K9q2EbF8a4MbK1ZR'  # ID папки на Google Диске, которую вы хотите скачать
+
+
+# Укажите путь к файлу JSON вашего служебного аккаунта и область доступа
+SERVICE_ACCOUNT_FILE = path_to_key + secret_key
+SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
+
+# Создайте объект учетных данных с использованием файла служебного аккаунта и области доступа
+credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+# Создайте объект Google Drive API
+drive_service = build('drive', 'v3', credentials=credentials)
+
+
+folder_to_move = None
+dict_of_folder  = {}
+error_files  = {}
+
+# Запросите список файлов в указанной папке
+result = get_folder_files(folder_id)
+
+#перебираем список папок на удаленном диске и смотрим в нутри
+for file in result:
+    remoute_files = get_folder_files(file.get('id'))
+
+
+    for i in remoute_files:
+        if  pathtodownload.joinpath(file.get('name')).joinpath(i.get('name')).exists():
+            print(f"File {i.get('name')} in dir {file.get('name')} is exist")
+        else: 
+           error_files [i.get('name')] = i.get('id')
+
+
+    if pathtodownload.joinpath(file.get('name')).exists():
+        for i in pathtodownload.joinpath(file.get('name')).iterdir():
+            print(i)
+    else:
+        print(f"No file {file.get('name')}")
+    dict_of_folder [file.get('name')] = file.get('id')
+
+ 
+
+
+#качаем файлы
+for k,v in dict_of_folder.items():
+    print('Start to download')
+    #создаем папку если такая не существует
+    newfolderpath_name = Path(pathtodownload.joinpath(k))
+    if not newfolderpath_name.exists():
+        newfolderpath_name.mkdir()
+    #получаем список файлов папке
+    filenames = get_folder_files(v)
+    for file in filenames:
+
+        file_id = file['id']
+        file_name = file['name']
+        #удаляем из названия все лишнее иначе вин не создаст файл
+        new_file_name = "".join(c if c.isalnum() or c in (' ', '.', '-') else '_' for c in file_name)
+
+        request = drive_service.files().get_media(fileId=file_id)
+        fh = io.FileIO(newfolderpath_name / new_file_name, 'wb')
+        downloader = MediaIoBaseDownload(fh, request)
+        done = False
+        while done is False:
+            status, done = downloader.next_chunk()
+        print(f'Successfully downloaded: {file_name} Time to comlete {(time.time() - starttime) / 60} min')
+
+print(f"Finish. Time to comlete {(time.time() - starttime) / 60} min")
+
+
+ 
+
---
+++ filemover/main.py
@@ -0,0 +1,63 @@
+from zipfile import ZipFile
+from pathlib import Path
+from datetime import date, timedelta
+
+#скрипт для автоматической распаковки архива выгруженного сервиса Canva
+
+start_date = date(2023,10,29) #страрт -1день
+
+video_path = Path('D:/@START-UP/tenbox/')
+delta = 0
+
+# #Создаем первую папку в списке. дальше будет работать автоматически
+# pathtostring = str(start_date)
+# dir_to_create = video_path / pathtostring
+# if not dir_to_create.is_dir():
+#     dir_to_create.mkdir()
+#     print(f"Path {dir_to_create} is created")
+
+zipfile_name = None
+ 
+# Ищем существует ли зип файл
+for i in video_path.iterdir():
+    if i.suffix == '.zip':
+         zipfile_name = i
+
+if zipfile_name and zipfile_name.exists():
+    print('Zip file is found. Starting to extract')
+    with ZipFile(zipfile_name) as zipfile:
+        zipfile.extractall(video_path)
+    print("Extracted")
+    zipfile_name.unlink()
+    print("Zip file is delete")
+else:
+    print('No zip file')
+
+#Создаем директории и переносим файлы
+for i in range(100):
+    index = str(i+1)
+    sourse_file_pre = video_path.joinpath(index +'.mp4')
+    print(sourse_file_pre)
+
+    if sourse_file_pre.exists():
+        if i % 10 == 0 : #создаем папку на каждой итерации кратной 10
+            delta +=1
+            pathforfiles = start_date + timedelta(days=delta)
+            pathtostring = str(pathforfiles)
+            dir_to_create = video_path / pathtostring
+            
+            if not dir_to_create.is_dir():
+                dir_to_create.mkdir()
+                print(f"Path {pathforfiles} is created")
+    
+    
+        sourse_file = dir_to_create.joinpath(index +'.mp4')
+        print(sourse_file)
+        sourse_file_pre.replace(sourse_file)
+        print(f"Move from {sourse_file_pre} to {sourse_file}")
+    else:
+        print('No file to parce')
+
+
+        
+ 
